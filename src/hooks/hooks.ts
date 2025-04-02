77b429a07ff595fd1077f1ed718f78a3ed76b08c
duong2912211 <nguyenduongktpm3@gmail.com>
2025-04-02 22:21:57 +0700

    Update wrong config

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -8,7 +8,7 @@ let context: BrowserContext;
 let loginPage: LoginPage;
 
 BeforeAll(async function () {
-    browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: true });
 });
 
 Before(async function () {
